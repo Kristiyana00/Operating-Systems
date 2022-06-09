01c51e64dc0802c803a1b91b96420425d3e72656
Kristiyana Nikolova <57190703+Kristiyana00@users.noreply.github.com>
2022-06-09 09:55:30 +0300

    Update 23-2.sh

--- Exam-problems/23/23-2.sh
+++ Exam-problems/23/23-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while read u h; do
-        find $u -type f -printf"$u %p %T@\n|
+        find $h -type f -printf"$u %p %T@\n|
 
 done < <(cut -d ':' -f 1,6 /etc/passwd | tr ':' ' ') | sort -nr -k 3 | head -n 1 | awk '{print $1, $2}'
